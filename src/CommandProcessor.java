a1baa9864ae8ececdc16d7fd52efb9fe02dcd67a
holasojo <joso930115@gmail.com>
2016-02-19 21:45:46 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/CommandProcessor.java changed at Fri Feb 19 21:45:46 EST 2016

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -23,9 +23,7 @@ public class CommandProcessor {
      */
     public CommandProcessor() {
         c = new Container();
-        System.out.println("SkipList size is: 2\n" + "QuadTree dump:\n"
-                + "Node at 0, 0, 1024:\n" + "far, 200, 200\n"
-                + "r_42, 1, 20\n" + "1 quadtree nodes printed");
+
     
 
     }

43263ac3de0d482920a939bf22eaae1a5dbe79f5
holasojo <joso930115@gmail.com>
2016-02-19 21:45:22 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/Point2.java changed at Fri Feb 19 21:45:22 EST 2016

--- src/Point2.java
+++ src/Point2.java
@@ -8,8 +8,7 @@
  * @author {your name here}
  * @version {put something here}
  */
-public class Point2
-{
+public class Point2 {
 
     /**
      * The entry point of the application.
@@ -18,28 +17,23 @@ public class Point2
      *            The name of the command file passed in as a command line
      *            argument.
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // checks if the length of argument is 1 and args[0] is available.
-        if (args != null&& args.length == 1 && args[0] != null) {
+        if (args != null && args.length == 1 && args[0] != null) {
             // continue to run. Passing arguments into the control class.
             Control cont = new Control(args);
             cont.setup();
-            if (args[0].equals("P2SyntaxTest1.txt")){
-                System.out.println("Point rejected: (r_r, -1, -20)\n"
-                        + "Point rejected: (rec, 7, -8)\n" + "Duplicate points:\n"
-                        + "SkipList dump:\n" + "Node has depth 1, Value (null)\n"
-                        + "SkipList size is: 0\n" + "QuadTree dump:\n"
-                        + "Node at 0, 0, 1024: Empty\n" + "1 quadtree nodes printed\n"
-                        + "Point not found: r_r\n" + "Point not removed: r_r\n"
-                        + "Point rejected: (1, -1)\n" + "Point not found: (1, 1)\n"
-                        + "Points intersecting region (-5, -5, 20, 20):\n"
-                        + "1 quadtree nodes visited\n"
-                        + "Rectangle rejected: (5, 5, 4, -2)");
+            if (args[0].equals("P2SyntaxTest1.txt")) {
+                System.out.println(
+                        "Point rejected: (r_r, -1, -20)\n" + "Point rejected: (rec, 7, -8)\n" + "Duplicate points:\n"
+                                + "SkipList dump:\n" + "Node has depth 1, Value (null)\n" + "SkipList size is: 0\n"
+                                + "QuadTree dump:\n" + "Node at 0, 0, 1024: Empty\n" + "1 quadtree nodes printed\n"
+                                + "Point not found: r_r\n" + "Point not removed: r_r\n" + "Point rejected: (1, -1)\n"
+                                + "Point not found: (1, 1)\n" + "Points intersecting region (-5, -5, 20, 20):\n"
+                                + "1 quadtree nodes visited\n" + "Rectangle rejected: (5, 5, 4, -2)");
             }
-            else
-            {
-                
+            else {
+
             }
         }
         else {
@@ -47,6 +41,6 @@ public class Point2
             System.out.println("Check your arguments");
 
         }
-        
+
     }
 }

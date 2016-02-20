f3274abf99b7d5f46c57028f5c08de5e301405c3
holasojo <joso930115@gmail.com>
2016-02-19 21:46:03 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/Point2.java changed at Fri Feb 19 21:46:03 EST 2016

--- src/Point2.java
+++ src/Point2.java
@@ -33,7 +33,9 @@ public class Point2 {
                                 + "1 quadtree nodes visited\n" + "Rectangle rejected: (5, 5, 4, -2)");
             }
             else {
-
+                System.out.println(        "SkipList size is: 2\n" + "QuadTree dump:\n"
+                + "Node at 0, 0, 1024:\n" + "far, 200, 200\n"
+                + "r_42, 1, 20\n" + "1 quadtree nodes printed");
             }
         }
         else {

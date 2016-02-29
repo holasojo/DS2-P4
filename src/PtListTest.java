ce3ba6a47c6a8ec7f3a6ed83fb0a88288045fed3
holasojo <joso930115@gmail.com>
2016-02-29 12:28:40 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PtListTest.java changed at Mon Feb 29 12:28:40 EST 2016

--- src/PtListTest.java
+++ src/PtListTest.java
@@ -13,7 +13,21 @@ public class PtListTest extends student.TestCase {
     public void setUp(){
         list1 = new PtList();
         list2 = new PtList();
-        p1 = new Point("a",1,1);
+        p1 = new Point("A",1,1);
+        p2 = new Point("B",2,2);
+        p3 = new Point("C",1,1);
+        p4 = new Point("D",5,5);
+        p5 = new Point("F",0,0);
+        p6 = new Point("G",1023,1023);
+        
+    }
+    
+    public void testAppend(){
+        list1.append(p1);
+        list1.append(p2);
+        assertEquals(list1.size(),2);
+        assertEquals(list1.toString(), "A, 1, 1\nB, 2, 2\n");
+      
     }
 
 }

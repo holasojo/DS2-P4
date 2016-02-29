4cef90713382a57f93fc1899ba583ad2bf4d98de
holasojo <joso930115@gmail.com>
2016-02-29 12:17:34 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PtListTest.java changed at Mon Feb 29 12:17:34 EST 2016

---
+++ src/PtListTest.java
@@ -0,0 +1,22 @@
+
+public class PtListTest extends student.TestCase{
+    PtList list1;
+    PtList list2;
+    Point pt1;
+    Point pt2;
+    Point pt3;
+    
+    public void setUp(){
+        list1 = new PtList();
+        list2 = new PtList();
+        pt1 = new Point("a",1,1,3);
+        pt2 = new Point();
+        pt3 = new Point();
+        
+    }
+    
+    public void testAppend(){
+       
+    }
+
+}

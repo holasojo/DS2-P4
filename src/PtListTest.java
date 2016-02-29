c87c67963e375a0942fe20489e9a97ac26c9d858
holasojo <joso930115@gmail.com>
2016-02-29 12:47:34 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PtListTest.java changed at Mon Feb 29 12:47:34 EST 2016

--- src/PtListTest.java
+++ src/PtListTest.java
@@ -51,7 +51,7 @@ public class PtListTest extends student.TestCase {
         assertEquals(removed[0].toString(), "B, 2, 2");
         assertEquals(removed[2].toString(), "D, 5, 5");
         list1.append(new Point("B",2,2));
-        assertTrue(list1.checkAllSame(p2));
+        assertFalse(list1.checkAllSame(p2));
         
         
     }

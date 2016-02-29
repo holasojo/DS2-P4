a4071346492656693dd2342600809dcff0bbb820
holasojo <joso930115@gmail.com>
2016-02-29 12:55:11 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTree.java changed at Mon Feb 29 12:55:11 EST 2016

--- src/PRQuadTree.java
+++ src/PRQuadTree.java
@@ -2,13 +2,19 @@
 public class PRQuadTree {
     QuadNode root;
     int size;
+    int x;
+    int y;
+    int width;
     
-    public PRQuadTree(){
+    public PRQuadTree(int x1, int y1, int width1){
         root = IntlNode.flyweight();
         size = 0;    
+        x = x1;
+        y = y1;
+        width = width1;
     }
 
     public void insert(Point pt){
-        
+        root.insert(pt, x, y, width);
     }
 }

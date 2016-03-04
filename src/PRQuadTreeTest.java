40c285a2f9212fb93b631c23b01ce841b1901e95
holasojo <joso930115@gmail.com>
2016-03-03 20:05:04 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTreeTest.java changed at Thu Mar 03 20:05:04 EST 2016

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -96,6 +96,10 @@ public class PRQuadTreeTest extends student.TestCase {
         c.insert("r_r", -1, -20);
         c.insert("r_r", 7, -8);
         systemOut().clearHistory();
+        c.tree().dump();
+        assertFuzzyEquals(" Point rejected: (r_r, -1, -20)\n"
+                + "Point rejected: (rec, 7, -8)", systemOut().getHistory());
+       
 
     }
 

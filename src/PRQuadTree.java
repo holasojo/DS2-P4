e56d7fd9f20e823908a5920073f077684bf528c6
holasojo <joso930115@gmail.com>
2016-03-03 19:51:30 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTree.java changed at Thu Mar 03 19:51:30 EST 2016

--- src/PRQuadTree.java
+++ src/PRQuadTree.java
@@ -5,38 +5,38 @@ public class PRQuadTree {
     private int x;
     private int y;
     private int width;
-    
-    public PRQuadTree(int x1, int y1, int width1){
+
+    public PRQuadTree(int x1, int y1, int width1) {
         root = IntlNode.flyweight();
-        size = 0;    
+        size = 0;
         x = x1;
         y = y1;
         width = width1;
     }
 
-    public void insert(Point pt){
+    public void insert(Point pt) {
         root = root.insert(pt, x, y, width);
         size++;
     }
-    
-    public int size(){
+
+    public int size() {
         return size;
     }
-    
-    public void dump(){
+
+    public void dump() {
         System.out.println("QuadTree dump:");
         int nodeCount = root.dump(x, y, width, 0);
         System.out.println(nodeCount + " quadtree nodes printed");
-        
+
     }
-    
-    public QuadNode remove(int x, int y)
-    {   
-        Point pt = new Point("dummy",x,y);
-        QuadNode found = root.searchbyCoor(pt, x, y, width);
-        (found != null){
+
+    public QuadNode remove(int x, int y) {
+
+        Point found = root.searchbyCoor(new Point("dummy", x, y), x, y, width);
+        if (found != null) {
             root = root.remove(pt, x, y, width);
         }
-        
+        return root;
+
     }
 }

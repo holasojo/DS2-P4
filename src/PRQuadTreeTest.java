d889f15621e613e59fe58afbf337763ded86053a
holasojo <joso930115@gmail.com>
2016-03-03 21:07:26 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTreeTest.java changed at Thu Mar 03 21:07:26 EST 2016

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -153,9 +153,8 @@ public class PRQuadTreeTest extends student.TestCase {
         c.insert("A", 0, 0);
         c.insert("A", 0, 0);
         c.insert("A", 1, 1);
-        c.insert("B", 400, 600);
-        c.insert("A", 200, 200);
         c.tree().dump();
+        c.removebyCoor(1, 1);
         
     }
 

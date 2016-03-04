f676e525de88aba9445fadd6a64aed12cf879d9f
holasojo <joso930115@gmail.com>
2016-03-03 19:56:11 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTreeTest.java changed at Thu Mar 03 19:56:11 EST 2016

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -10,10 +10,12 @@ public class PRQuadTreeTest extends student.TestCase {
 
     public void testInsert1() {
         c.tree().dump();
-        assertFuzzyEquals("Node at 0, 0, 1024: Internal", systemOut().getHistory());
+        assertFuzzyEquals("quadtree dump\nnode at 0 0 1024 empty\n1 quadtree nodes printed", systemOut().getHistory());
+        systemOut().clearHistory();
         c.insert("A", 0, 0);
         assertEquals(c.tree().size(), 1);
-        c.tree().dump();
+        assertFuzzyEquals("quadtree dump\nnode at 0 0 1024 empty\n1 quadtree nodes printed", systemOut().getHistory());
+        systemOut().clearHistory();
         c.insert("B", 1, 1);
         c.tree().dump();
         c.insert("C", 1000, 1000);

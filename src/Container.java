72a1a54c89ea3764f5c8350f73cb436b85d57db9
holasojo <joso930115@gmail.com>
2016-03-08 13:04:37 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/Container.java changed at Tue Mar 08 13:04:37 EST 2016

--- src/Container.java
+++ src/Container.java
@@ -116,7 +116,7 @@ public class Container {
      */
     public boolean fits(int x, int y, int w, int h) {
 
-        return (w > 0 && h > 0)
+        return (w >= 0 && h >= 0)
                 && ((x < worldWidth) && (y < worldWidth));
 
     }

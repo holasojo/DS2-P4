fe1b923c2bc004f324b195a91cfe93fe139faf83
holasojo <joso930115@gmail.com>
2016-03-07 20:44:07 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/Point2Test.java changed at Mon Mar 07 20:44:06 EST 2016

--- src/Point2Test.java
+++ src/Point2Test.java
@@ -4,31 +4,25 @@ import student.TestCase;
  * @author CS3114 staff
  * @version 2
  */
-public class Point2Test
-    extends TestCase
-{
+public class Point2Test extends TestCase {
 
     /**
      * This method sets up the tests that follow.
      */
-    public void setUp()
-    {
+    public void setUp() {
         // Nothing here yet
     }
-
 
     // ----------------------------------------------------------
     /**
      * This method is simply to get code coverage of the class declaration.
      */
-    public void testRInit()
-    {
+    public void testRInit() {
         Point2 dum = new Point2();
         assertNotNull(dum);
         Point2.main(null);
         assertFuzzyEquals("Hello, World\n", systemOut().getHistory());
     }
-
 
     // ----------------------------------------------------------
     /**
@@ -36,14 +30,11 @@ public class Point2Test
      *
      * @throws Exception
      */
-    public void testSyntax1()
-        throws Exception
-    {
+    public void testSyntax1() throws Exception {
         String[] args = new String[1];
         args[0] = "P2SyntaxTest1.txt";
         Point2.main(args);
-        assertEquals(
-            "Point rejected: (r_r, -1, -20)\n"
+        assertEquals("Point rejected: (r_r, -1, -20)\n"
                 + "Point rejected: (rec, 7, -8)\n" + "Duplicate points:\n"
                 + "SkipList dump:\n" + "Node has depth 1, Value (null)\n"
                 + "SkipList size is: 0\n" + "QuadTree dump:\n"
@@ -53,26 +44,22 @@ public class Point2Test
                 + "Points intersecting region (-5, -5, 20, 20):\n"
                 + "1 quadtree nodes visited\n"
                 + "Rectangle rejected: (5, 5, 4, -2)\n",
-            systemOut().getHistory());
+                systemOut().getHistory());
     }
 
-
-// ----------------------------------------------------------
+    // ----------------------------------------------------------
     /**
      * Simple test of all the "good" command outputs
      *
      * @throws Exception
      */
-    public void testSyntax2()
-        throws Exception
-    {
+    public void testSyntax2() throws Exception {
         String[] args = new String[1];
         args[0] = "P2SyntaxTest2.txt";
         Point2.main(args);
-        assertTrue(
-            systemOut().getHistory()
+        assertTrue(systemOut().getHistory()
                 .endsWith("SkipList size is: 2\n" + "QuadTree dump:\n"
-                    + "Node at 0, 0, 1024:\n" + "far, 200, 200\n"
-                    + "r_42, 1, 20\n" + "1 quadtree nodes printed\n"));
+                        + "Node at 0, 0, 1024:\n" + "far, 200, 200\n"
+                        + "r_42, 1, 20\n" + "1 quadtree nodes printed\n"));
     }
 }

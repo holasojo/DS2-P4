211591262feb632950f29dd9b7f5e28e66eccd8d
holasojo <joso930115@gmail.com>
2016-03-10 23:45:22 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PRQuadTreeTest.java changed at Thu Mar 10 23:44:27 EST 2016

--- src/PRQuadTreeTest.java
+++ src/PRQuadTreeTest.java
@@ -253,6 +253,7 @@ public class PRQuadTreeTest extends student.TestCase {
      */
     public void testRegionSearch1() {
         c.insert("A", 1, 20);
+       
         c.insert("B", 10, 30);
         c.insert("C", 700, 800);
         c.insert("D", 200, 800);
@@ -299,6 +300,19 @@ public class PRQuadTreeTest extends student.TestCase {
                         + "found: (D, 200, 800)\n2 quadtree nodes visited",
                 systemOut().getHistory());
 
+    }
+    
+    public void testRegionSearch2(){
+        
+        c.insert("A", 5, 5);
+        c.insert("B", 30, 30);
+        systemOut().clearHistory();
+        c.regionSearch(5, 5, 25, 25);
+        assertFuzzyEquals(
+                "Points intersecting region (0, 700, 300, 300):\nPoint "
+                        + "found: (D, 200, 800)\n2 quadtree nodes visited",
+                systemOut().getHistory());
+        
     }
 
 }

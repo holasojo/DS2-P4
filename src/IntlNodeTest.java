afa8cd2974d8563526ca53d8843147b23fd23e54
holasojo <joso930115@gmail.com>
2016-03-11 16:24:14 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/Point.java changed at Fri Mar 11 16:24:13 EST 2016

--- src/IntlNodeTest.java
+++ src/IntlNodeTest.java
@@ -1,30 +1,33 @@
 /**
  * Testing Internal Node
+ * 
  * @author sohyun
  * @author sshumway
  * @version 3/7/2016
  */
 public class IntlNodeTest extends student.TestCase {
-    
+
     private IntlNode internal;
-    
-    /**setup
+
+    /**
+     * setup
      * 
      */
-    public void setUp()
-    {
-        //empty
+    public void setUp() {
+        // empty
         internal = new IntlNode();
     }
-    
+
     /**
-     * check to see if the flyweight in internal node is the same as 
+     * check to see if the flyweight in internal node is the same as
      * Flyweight.class
      */
-    public void testFlyweight()
-    {
-        
+    public void testFlyweight() {
+
         assertEquals(IntlNode.flyweight().getClass(), Flyweight.class);
+        assertEquals(IntlNode.flyweight()
+                .remove(new Point(1, 1), 0, 0, 512, false).getClass(),
+                Flyweight.class);
 
     }
 

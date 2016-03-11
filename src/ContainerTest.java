9943227aa0e41d97f55be2db5b86a6016022f36b
holasojo <joso930115@gmail.com>
2016-03-11 14:56:32 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/ContainerTest.java changed at Fri Mar 11 14:56:32 EST 2016

--- src/ContainerTest.java
+++ src/ContainerTest.java
@@ -39,6 +39,7 @@ public class ContainerTest extends student.TestCase {
     {
         
         assertFalse(box.regionSearch(100, 200, -10, 12));
+        assertFalse(box.regionSearch(100, 200, -10, -12));
         assertFalse(box.regionSearch(100, 200, 20, -5));
         assertFalse(box.regionSearch(-10, 200, 5, 10));
         assertFalse(box.regionSearch(10, -5, 20, -2));

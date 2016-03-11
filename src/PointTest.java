37692de54da0b39e71881c51096bc4eea69c6517
holasojo <joso930115@gmail.com>
2016-03-11 18:11:45 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/PointTest.java changed at Fri Mar 11 18:11:45 EST 2016

--- src/PointTest.java
+++ src/PointTest.java
@@ -121,6 +121,15 @@ public class PointTest extends student.TestCase {
         //
         // return Direction.SE;
 
+        assertEquals(pt6.quadrant(8, 8), Direction.SE);
+        assertEquals(pt7.quadrant(8, 8), Direction.SE);
+        assertEquals(pt8.quadrant(8, 8), Direction.SE);
+        assertEquals(pt9.quadrant(8, 8), Direction.SE);
+        assertEquals(pt10.quadrant(8, 8), Direction.SE);
+        assertEquals(pt11.quadrant(8, 8), Direction.SE);
+        assertEquals(pt12.quadrant(8, 8), Direction.SE);
+        assertEquals(pt13.quadrant(8, 8), Direction.SE);
+        assertEquals(pt14.quadrant(8, 8), Direction.SE);
         assertEquals(pt15.quadrant(8, 8), Direction.SE);
 
     }

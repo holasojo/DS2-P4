9ddceae31ceb1ba481eb815bacc50df8868f1d2f
holasojo <joso930115@gmail.com>
2016-03-11 15:23:12 -0500

    File /Users/SOJO/Documents/eclipse_workspace/Assignment2/src/ContainerTest.java changed at Fri Mar 11 15:23:12 EST 2016

--- src/ContainerTest.java
+++ src/ContainerTest.java
@@ -7,23 +7,20 @@
  *
  */
 public class ContainerTest extends student.TestCase {
-    
+
     Container box;
-    
+
     /**
      * Sets up the test fixture.
      */
-    public void setUp()
-    {
+    public void setUp() {
         box = new Container();
     }
-    
-    
+
     /**
      * tests the fits method to see if points fit in the "world".
      */
-    public void testFits()
-    {
+    public void testFits() {
         box.insert("sam", -20, 1);
         box.insert("sam", 20, 45);
         box.insert("sam", 20, 45);
@@ -31,19 +28,21 @@ public class ContainerTest extends student.TestCase {
         box.insert("sam", 20, 5000);
         assertEquals(box.getList().size(), 2);
     }
-    
+
     /**
      * Tests regionSearch.
      */
-    public void testRegionSearch()
-    {
-        //(w > 0 && h > 0) && (x + w > 0) && (y + h > 0);
-        //(w > 0 && h > 0) 
-        //(w > 0 || h > 0) 
+    public void testRegionSearch() {
+        // (w > 0 && h > 0) && (x + w > 0) && (y + h > 0);
+        // (w > 0 && h > 0)
+        systemOut().clearHistory();
+        assertFalse(box.regionSearch(100, 200, 10, 12));
+        assertFuzzyEquals("Rectangle Rejected (100, 200, -10, 12)",
+                systemOut().getHistory());
+        // (w > 0 || h > 0)
         systemOut().clearHistory();
         assertFalse(box.regionSearch(100, 200, -10, 12));
-        assertFuzzyEquals(
-                "Rectangle Rejected (100, 200, -10, 12)",
+        assertFuzzyEquals("Rectangle Rejected (100, 200, -10, 12)",
                 systemOut().getHistory());
         assertFalse(box.regionSearch(100, 200, -10, -12));
         assertFalse(box.regionSearch(100, 200, 20, -5));
@@ -52,27 +51,24 @@ public class ContainerTest extends student.TestCase {
         assertFalse(box.regionSearch(0, 0, 0, 0));
         assertFalse(box.regionSearch(-200, -200, -10, -10));
         assertNotNull(box.getList());
-        
+
         systemOut().clearHistory();
-        
-   
+
     }
-    
-    
+
     /**
      * Tests the skiplist.
      */
-    public void testSkipList()
-    {
-        KVPair<String, Point> it = new KVPair<String, 
-                Point>("Bob", new Point("Bob", 10, 20));
+    public void testSkipList() {
+        KVPair<String, Point> it = new KVPair<String, Point>("Bob",
+                new Point("Bob", 10, 20));
         assertNull(box.getList().remove(it));
         box.getList().insert(it);
-        KVPair<String, Point> dummy = new KVPair<String , 
-                Point>("Scott", new Point("Scott", 10, 20));
+        KVPair<String, Point> dummy = new KVPair<String, Point>("Scott",
+                new Point("Scott", 10, 20));
         assertNull(box.getList().remove(dummy));
-        
-        box.getList().remove(new Point("asdf", 21, 20)); 
+
+        box.getList().remove(new Point("asdf", 21, 20));
         box.getList().remove(new Point("asdffff", 100, 100));
     }
 
